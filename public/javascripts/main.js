39904c8717a86750154758b8f679fff5502fabee
Beatriz Duarte <beatriznduarte1@gmail.com>
2019-11-16 00:20:32 -0300

    desenvolvimento da requisição ajax do arquivo .json

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,49 @@
 // your code
+(function readyJS(win,doc){
+
+    'use strict';
+
+    let btn = doc.querySelector('#btndb');
+
+    function ranking(){
+        let ajax = new XMLHttpRequest();
+        ajax.open('GET','../fazenda.json');
+        ajax.onreadystatechange = () =>{
+            if(ajax.status === 200 && ajax.readyState === 4){
+                //document.getElementById('render').innerHTML = ajax.responseText;
+                let res = JSON.parse(ajax.responseText);
+                let farmers = res.data
+
+                function getFullName(item) {
+                    let fullname = item.name;
+                    console.log(fullname)
+                    return fullname;
+                }
+
+                function getImg(img){
+                    let photo = img.picture;
+                    console.log(photo)
+                    return photo;
+                }
+
+                function getDescription(descri){
+                    let descriptions = descri.description;
+                    console.log(descriptions)
+                    return descriptions;
+                }
+                
+                document.getElementById('name').innerHTML = farmers.map(getFullName);
+                document.getElementById('img').innerHTML = farmers.map(getImg);
+                document.getElementById('description').innerHTML = farmers.map(getDescription);
+                //for(let i=0;i < farmers.length; i++){
+                    //console.log(farmers[i].name);
+                    //document.getElementById('#render').innerHTML = 
+                //}
+            }
+        }
+        ajax.send()
+    
+    }
+    btn.addEventListener('click', ranking, false);
+
+})(window,document);
